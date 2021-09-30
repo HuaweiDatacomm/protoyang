2c85e951929a8b168952b30fecbfd4593d1d156a
lllyfeng <frank.fengchong@huawei.com>
2021-09-30 16:40:46 +0800

    modify setup.py and rename src to protoyang

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='protoyang',
-    version='0.0.1beta',
-    packages=['src', 'src.hpproto'],
+    version='0.0.2',
+    packages=['protoyang', 'protoyang.hpproto'],
     url='https://github.com/HuaweiDatacomm/protoyang',
     license='Apache License, Version 2.0',
-    author='f00360218',
+    author='feng chong',
     author_email='frank.fengchong@huawei.com',
     description='a tool generate .proto files according yang modules'
 )
